b9823240e97b8683b2e9a9cc9f4f2c02b9538a97
anewholm <annesley_newholm@yahoo.it>
2024-12-10 19:51:27 +0300

    'no_message'

--- SQL/table-alters.sql
+++ SQL/table-alters.sql
@@ -1,2 +1,18 @@
-select concat('alter table ', table_name, ' add column ...;') 
-from information_schema.tables where table_name like('acorn%')
+do $$
+begin
+execute (
+	select string_agg(concat('alter table if exists ', table_schema, '.', table_name, ' ', '
+	    ADD CONSTRAINT created_at_event_id FOREIGN KEY (created_at_event_id)
+	    REFERENCES public.acorn_calendar_event (id) MATCH SIMPLE
+	    ON UPDATE NO ACTION
+	    ON DELETE NO ACTION
+	    NOT VALID;
+	CREATE INDEX IF NOT EXISTS fki_created_at_event_id
+	    ON ', table_schema, '.', table_name, '(created_at_event_id)'),
+	';
+	
+	') 
+	from information_schema.tables 
+	where table_name like('acorn_lojistiks_%')
+);
+end$$;
