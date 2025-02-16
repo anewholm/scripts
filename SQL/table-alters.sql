ccec2f00e6d03a3372f205fa4ef5c78e524205b4
anewholm <annesley_newholm@yahoo.it>
2025-02-16 17:16:45 +0300

    'no_message'

--- SQL/table-alters.sql
+++ SQL/table-alters.sql
@@ -1,18 +1,8 @@
 do $$
 begin
 execute (
-	select string_agg(concat('alter table if exists ', table_schema, '.', table_name, ' ', '
-	    ADD CONSTRAINT created_at_event_id FOREIGN KEY (created_at_event_id)
-	    REFERENCES public.acorn_calendar_event (id) MATCH SIMPLE
-	    ON UPDATE NO ACTION
-	    ON DELETE NO ACTION
-	    NOT VALID;
-	CREATE INDEX IF NOT EXISTS fki_created_at_event_id
-	    ON ', table_schema, '.', table_name, '(created_at_event_id)'),
-	';
-	
-	') 
+	select string_agg(concat('alter table if exists ', table_schema, '.', table_name, ' owner to justice'), ';') 
 	from information_schema.tables 
-	where table_name like('acorn_lojistiks_%')
+	where table_name like('acorn_university_%')
 );
 end$$;
