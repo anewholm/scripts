1f7e7f7be3b0e7d58f93ddd6bfa6e00caf998f75
anewholm <annesley_newholm@yahoo.it>
2025-10-21 09:21:40 +0300

    'no_message'

--- SQL/databases/example.sql
+++ SQL/databases/example.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict IpYZSbPRcCeeYph4B4qq6NEqqhj1bd3cj8KMTdSAbRkiLRqfIs57V7uS4PlPWOr
+\restrict WNKcPgTeltHdB4nuXnuLTxHX1uY4vyrFGflwe7E9D1yFuWH2EmGCFZpEaIgrTEO
 
 -- Dumped from database version 16.10 (Ubuntu 16.10-1.pgdg24.04+1)
 -- Dumped by pg_dump version 16.10 (Ubuntu 16.10-1.pgdg24.04+1)
@@ -19308,5 +19308,5 @@ GRANT ALL ON SEQUENCE public.system_settings_id_seq TO token_8_no WITH GRANT OPT
 -- PostgreSQL database dump complete
 --
 
-\unrestrict IpYZSbPRcCeeYph4B4qq6NEqqhj1bd3cj8KMTdSAbRkiLRqfIs57V7uS4PlPWOr
+\unrestrict WNKcPgTeltHdB4nuXnuLTxHX1uY4vyrFGflwe7E9D1yFuWH2EmGCFZpEaIgrTEO
 
